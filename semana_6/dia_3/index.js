340ced0ae548a3b308f00201e094cdedce90d9ce
Dante Guzman <dante.guzmanceballos@gmail.com>
2018-10-17 19:50:56 -0500

    semana6

---
+++ semana_6/dia_3/index.js
@@ -0,0 +1,11 @@
+let animals = document.querySelector('.amini')
+let darkside = document.querySelector('.dmini')
+let thewall = document.querySelector('.wmini')
+
+function cerebro() {
+
+    
+
+}
+
+boton.addEventListener('click', cerebro);
